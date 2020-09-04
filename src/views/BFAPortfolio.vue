b92d2c67ef8aa5abf1ccac1a1e8bcea6286c2165
“BANDER <Bandersaleh93@gmail.com>
2020-09-04 00:39:03 -0700

    Completed Middle/Top half of Portfolio Page, including html url

--- src/views/BFAPortfolio.vue
+++ src/views/BFAPortfolio.vue
@@ -17,16 +17,20 @@
       </div>
     </header>
 
+    <hr>
+
     <h1>Bander Saleh's BFA in Film Portfolio</h1>
+
+    <hr>
+
     <h2> Documentary Style: </h2>
     
     </h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/9YE5nNWrM0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="640" height="360" src="https://www.youtube.com/embed/9YE5nNWrM0I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </h2>
 
-    <h2>
+    <hr>
 
-    </h2>
 
     <h2>
       Motion Graphics/Graphic Design:
@@ -35,17 +39,18 @@
       <iframe src="https://player.vimeo.com/video/55433797" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
     </h2>
 
-     <h2>
+    <hr>
 
-    </h2>
 
     <h2>
       Extreme Sports experience:
     </h2>
 
     <h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Cz88o7kVEEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="640" height="360" src="https://www.youtube.com/embed/Cz88o7kVEEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </h2>
+
+    <hr>
 
     <h2>
       Youtube: Youtube.com/bandersaleh
@@ -56,6 +61,8 @@
     <h2>
     Instagram: instagram.com/bandersaleh93
   </h2>
+
+  <!-- <span class="subheading">[ Location: Silicon Valley, California, USA ].</span> -->
 
     
 
