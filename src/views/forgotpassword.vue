33286c9ca829b3b57390f2b8b5eaf72c2c758044
“BANDER <Bandersaleh93@gmail.com>
2020-09-08 04:32:49 -0700

    Frontend Version 1 Live on Heroku Deployment

--- src/views/forgotpassword.vue
+++ src/views/forgotpassword.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="forgotpassword">
-
     <!-- Page Header -->
     <header class="masthead" style="background-image: url('img/post-bg.jpg')">
       <div class="overlay"></div>
@@ -8,18 +7,17 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="post-heading">
-              <h1>Man must explore, and this is exploration at its greatest</h1>
-              <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+              <h1>Lost Password?</h1>
+              <!-- <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
               <span class="meta">Posted by
                 <a href="#">Start Bootstrap</a>
-                on August 24, 2019</span>
+                on August 24, 2019</span> -->
             </div>
           </div>
         </div>
       </div>
     </header>
 
-  
     <form v-on:submit.prevent="submit()">
       <div class="col-lg-8 col-md-10 mx-auto">
         <h1>Forgot Password?</h1>
@@ -28,23 +26,20 @@
         </ul>
         <div class="form-group">
           <label>Email:</label>
-          <input type="email" class="form-control" v-model="email">
+          <input type="email" class="form-control" v-model="email" />
         </div>
-        
-        <input type="emailpw" class="btn btn-primary" value="Send">
+
+        <input type="emailpw" class="btn btn-primary" value="Send" />
       </div>
     </form>
   </div>
-  
-
-  
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  data: function () {
+  data: function() {
     return {
       email: "",
       password: "",
@@ -52,7 +47,7 @@ export default {
     };
   },
   methods: {
-    submit: function () {
+    submit: function() {
       var params = {
         email: this.email,
         password: this.password,
